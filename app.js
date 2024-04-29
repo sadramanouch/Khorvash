12eed18785a3d566f2092d862fb731d4c40acd5d
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-04-28 22:13:12 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,18 @@
       document.body.classList.toggle("light-mode");
   })
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all divs with class 'blog'
+    const blogDivs = document.querySelectorAll(".blog");
+
+    // Attach click event to each div
+    blogDivs.forEach((div) => {
+        const link = div.getAttribute("data-link"); // Get the URL from the data-link attribute
+        if (link) {
+            div.addEventListener("click", () => {
+                window.open(link, "_blank"); // Open the link in a new tab
+            });
+        }
+    });
+});
